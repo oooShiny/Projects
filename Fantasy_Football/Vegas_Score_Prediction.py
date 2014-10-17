61ca81987f1472a9784c46e2468d9bba850a3dda
Rich Barton <rbarton@corsis.com>
2014-10-17 11:30:11 -0400

    Cleaned up Vegas_Score_Prediction.py

--- Fantasy_Football/Vegas_Score_Prediction.py
+++ Fantasy_Football/Vegas_Score_Prediction.py
@@ -8,65 +8,69 @@ import string
 from collections import OrderedDict
 
 class info:
-	def __init__(self, favorite, underdog, linetotal, line, total):
+	def __init__(self, favorite, underdog, linetotal, line, total, matchups, favpoints, undpoints, finalfav, finalund):
 		self.favorite = favorite
 		self.underdog = underdog
 		self.linetotal = linetotal
-		self.line = linetotal[::2]
-		self.total = linetotal[1::2]
+		self.line = line
+		self.total = total
+		self.matchups = matchups
+		self.favpoints = favpoints
+		self.undpoints = undpoints
+		self.finalfav = finalfav
+		self.finalund = finalund
 
-"""Set up website to be parsed"""
-opener = urllib2.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-url = "http://www.footballlocks.com/nfl_lines.shtml"
-ourUrl = opener.open(url).read()
-soup = BeautifulSoup(ourUrl)
+info = info([],[],[],[],[],OrderedDict({}),[],[],[],[])
 
-tds = []
-date = []
-home = []
-linetotal = []
-line = []
-total = []
-away = []
-matchups = OrderedDict({})
-homenumber = []
-awaynumber = []
-homesemi = []
-awaysemi =[]
-
-
-
-for i in soup.findAll('p'):
-	for j in i.findAll('td'):
-		if j:
-			tds.append(j)
-for i,j in enumerate(tds):
-	if " ET" in j.text:
-		date.append(j.text)
-	if "-" in j.text:
-		home.append(tds[i-1].text)
-		away.append(tds[i+1].text)
-	if re.match("^[0-9.-]+$", j.text):
-		linetotal.append(float(j.text))
+def gather():
+	"""Set up website to be parsed"""
+	opener = urllib2.build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+	url = "http://www.footballlocks.com/nfl_lines.shtml"
+	ourUrl = opener.open(url).read()
+	soup = BeautifulSoup(ourUrl)	
 	
+	"""Parse website"""
+	tds = []
+	for i in soup.findAll('p'):
+		for j in i.findAll('td'):
+			if j:
+				tds.append(j)
+	
+	"""Put information in class"""
+	for i,j in enumerate(tds):
+		if "-" in j.text:
+			info.favorite.append(tds[i-1].text)
+			info.underdog.append(tds[i+1].text)
+		if re.match("^[0-9.-]+$", j.text):
+			info.linetotal.append(float(j.text))
+	info.line = info.linetotal[::2]
+	info.total = info.linetotal[1::2]
 
-line = linetotal[::2]
-total = linetotal[1::2]
+def modify():
+	"""Modify data for output"""
+	for i,j in zip(info.favorite, info.underdog):
+		info.matchups[i] = j
+	for i,j in zip(info.line, info.total):
+		info.favpoints.append(round(j/2 - i, 0))
+		info.undpoints.append(round(j/2 + i, 0))
+	for i,j in zip(info.favorite, info.favpoints):
+		info.finalfav.append("%s (%s) " % (i, j))
+	for i,j in zip(info.underdog, info.undpoints):
+		info.finalund.append("%s (%s) " % (i, j))
 
-for i,j in zip(home, away):
-	matchups[i] = j
-for i,j in zip(line, total):
-	homenumber.append(round(j/2 - i, 0))
-	awaynumber.append(round(j/2 + i, 0))
-for i,j in zip(home, homenumber):
-	homesemi.append("%s (%s) " % (i, j))
+def final():
+	"""Print final data"""
+	for i,j, in zip(info.finalund, info.finalfav):
+		if "At " in j:
+			print i + j
+		else:
+			print j + i
 
-for i,j in zip(away, awaynumber):
-	awaysemi.append("%s (%s) " % (i, j))
+def main():
+	gather()
+	modify()
+	print "Vegas Score Predictions\n"
+	final()
 
-for i,j, in zip(awaysemi, homesemi):
-	if "At " in j:
-		print i + j
-	else:
-		print j + i
+if  __name__ =='__main__':main()
